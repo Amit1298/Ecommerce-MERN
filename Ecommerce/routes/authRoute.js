eea48cd0e785bf7b43fb6a2193e109f7f641e9d0
Amit <amitkr24031995@gmail.com>
2023-05-25 23:19:32 +0530

    first commit

---
+++ Ecommerce/routes/authRoute.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const {
+  createUser,
+  loginUserCtrl,
+  getallUser,
+  getaUser,
+  deleteaUser,
+  updatedaUser,
+  blockUser,
+  unblockUser,
+  handleRefreshToken,
+  logout,
+  updatePassword,
+  forgotPasswordToken,
+} = require("../controller/userCtrl");
+const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
+const router = express.Router();
+
+router.post("/register", createUser);
+router.post("/forgot-password-token", forgotPasswordToken);
+router.put("/password", authMiddleware, updatePassword);
+router.post("/login", loginUserCtrl);
+router.get("/all-users", getallUser);
+router.get("/:id", authMiddleware, isAdmin, getaUser);
+router.get("/refresh", handleRefreshToken);
+router.get("/logout", logout);
+
+router.delete("/:id", deleteaUser);
+router.put("/edit-user", authMiddleware, updatedaUser);
+router.put("/block-user/:id", authMiddleware, isAdmin, blockUser);
+router.put("/unblock-user/:id", authMiddleware, isAdmin, unblockUser);
+
+module.exports = router;
